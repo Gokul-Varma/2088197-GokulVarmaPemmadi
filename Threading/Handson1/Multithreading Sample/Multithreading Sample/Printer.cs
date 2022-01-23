07d3a91ad6d8799c703c71f8f4f593a4f1f9accf
Gokul-Varma <gokulvarma685@gmail.com>
2022-01-24 00:01:23 +0530

    thread async service

---
+++ Threading/Handson1/Multithreading Sample/Multithreading Sample/Printer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+using System.Threading;
+
+
+
+namespace Multithreading_Sample
+{
+    class Printer
+    {
+        private object lockTocken = new object();
+
+
+
+        public void PrintNumbers()
+        {
+            lock (lockTocken)
+            {
+                Console.WriteLine("Thread-> {0} started @{1} and executing PrintNumbers() method",
+                Thread.CurrentThread.ManagedThreadId, DateTime.Now.ToLongTimeString());
+
+
+
+                Console.Write("Your numbers:");
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.Write("{0} ,", i);
+                    Thread.Sleep(1000);
+
+
+
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Threading/Handson1/ThreadStartSample/ThreadStartSample/Printer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadStartSample
+{
+    public class Printer
+    {
+        public void PrintNumbers()
+        {
+            Console.WriteLine($"-> {Thread.CurrentThread.Name} is executing PrintNumbers()");
+
+            Console.WriteLine("Your numbers: ");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"{i}, ");
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine();
+        }
+    }
+}
