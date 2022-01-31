749aedf6ded4a8bbb6306c07f5d94bf6cf5a31bc
Gokul-Varma <gokulvarma685@gmail.com>
2022-01-31 15:29:59 +0530

    added remaining

---
+++ Async/Handson1/AsyncAwaitUsage-1/AsyncAwaitUsage-1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace AsyncAwaitUsage_1
+{
+    public class Program
+    {
+        static async Task  Main(string[] args)
+        {
+            await Method1();
+           // Console.WriteLine("after the method1 fully completed ");
+
+        }
+        static async Task  Method1()
+        {
+            string str = await Method2();
+            Console.WriteLine(str); 
+         //   Console.WriteLine("method 1 completd");
+        }
+        static async Task <string> Method2()
+
+        {
+            Thread.Sleep(2000);
+            return "method2 displayed";
+
+        }
+    }
+}
---
+++ Practise check/GenericMedicine/CartonTest/CartonTests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System.IO;
+using System;
+using GenericMedicine;
+
+namespace CartonTest
+{
+    [TestFixture]
+    class CartonTests
+    {
+        Program obj = new Program();
+
+        [TestCase(20, "05/01/2022", "44-20-1, delhi")]
+        [TestCase(100, "05/010/2021", "44-89-6, mumbail")]
+        public void CartonObjectCreation(int medicineStripCount, DateTime launchDate, string retailerAddress)
+        {
+            Medicine med = obj.CreateMedicineDetail("balu", "dolo65", "carbon iron", DateTime.Parse("12/1/2022"), 2.3);
+            Assert.DoesNotThrow(() => obj.CreateCartonDetail(medicineStripCount,launchDate,retailerAddress,med));
+        }
+
+        [TestCase(-2, "12/23/2021", "44-20-1, delhi")]
+        [TestCase(-100, "12/23/2021", "44-89-6, mumbail")]
+        public void LessThanZeroStripCount(int medicineStripCount, DateTime launchDate, string retailerAddress)
+        {
+            Medicine med = obj.CreateMedicineDetail("abhi", "dolo65", "carbon iron", DateTime.Parse("12/12/2022"), 2.3);
+   
+            var ex = Assert.Throws<Exception>(() => obj.CreateCartonDetail(medicineStripCount, launchDate, retailerAddress, med));
+            Assert.That(ex.Message, Is.EqualTo("Incorrect strip count. Please check"));
+        }
+
+        [TestCase(2, "12/23/2000", "44-89-6, mumbail")]
+        [TestCase(100, "12/23/1900", "44-88-6, delhi")]
+        public void LaunchDateCheck(int medicineStripCount, DateTime launchDate, string retailerAddress)
+        {
+            Medicine med = obj.CreateMedicineDetail("ram", "paracetmol", "carbon iron", DateTime.Parse("12/1/2022"), 2.3);
+
+            var ex = Assert.Throws<Exception>(() => obj.CreateCartonDetail(medicineStripCount, launchDate, retailerAddress, med));
+            Assert.That(ex.Message, Is.EqualTo("Incorrect launch date. Please provide valid value"));
+        }
+
+        [TestCase(2, "12/23/2022", "44-89-6, mumbail")]
+        [TestCase(100, "12/23/2022", "44-89-6, hyderabad")]
+        public void NullCartonObject(int medicineStripCount, DateTime launchDate, string retailerAddress)
+        {
+            Assert.AreEqual(obj.CreateCartonDetail(medicineStripCount, launchDate, retailerAddress, null),null);
+        }
+    }
+}
